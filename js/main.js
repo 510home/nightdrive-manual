ed15611118452299e0c23007ef65d0ddc6f521b6
510home <daniel@danielcardozo.com>
2026-04-29 00:14:58 -0700

    Adjust camera Y position in main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ scene.fog = new THREE.Fog(0x000000, 2, 10);
 
 //create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(1.5, 2.2, .75);
+    camera.position.set(1.5, 1.75, .75);
     camera.lookAt(0.76, 0.2, -0.55);
 
 // create a renderer
