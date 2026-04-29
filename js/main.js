efa7035ed144c7ce40fda18dc824286f49932836
510home <daniel@danielcardozo.com>
2026-04-28 22:43:20 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -132,12 +132,8 @@ function animate() {
 const currentTime = Date.now()
 const deltaTime = currentTime - time
 time = currentTime
-//  if (wheelsFront) 
-//  {
-//   wheelsFront.rotation.z += 0.01;
-//  };
-//if (wheelsFront) {wheelsFront.rotation.z = t * Math.PI * 2 * 9;
-wheelsBack.rotation.z -= 0.02 * deltaTime;
+wheelsBack.rotation.z -= 0.08 * deltaTime;
+wheelsFront.rotation.z -= 0.08 * deltaTime;
 // controls.update();
 renderer.render(scene, camera);
  }
