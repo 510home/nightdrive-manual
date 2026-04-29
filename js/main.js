ec05a223655a9cb96443e34e65587038729b7ed7
510home <daniel@danielcardozo.com>
2026-04-29 11:41:50 -0700

    Change camera lookAt position in main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ scene.fog = new THREE.Fog(0x000000, 1, 8);
 //create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
     camera.position.set(2, 2, 2);
-    camera.lookAt(65, 25, -1);
+    camera.lookAt(-65, 2, -1);
 
 // create a renderer
 const renderer = new THREE.WebGLRenderer( { alpha: true });
