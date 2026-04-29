7c44a72d110826d9a1a043640308f47e76660718
510home <daniel@danielcardozo.com>
2026-04-29 11:18:38 -0700

    Adjust fog and camera settings in main.js

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 // create a scene - Fog is set to temp colors for now
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000022);
-scene.fog = new THREE.Fog(0x000000, 2, 10);
+scene.fog = new THREE.Fog(0x000000, 1, 8);
 
 //create a camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
     camera.position.set(2, 2, 2);
-    camera.lookAt(25, 2, -1);
+    camera.lookAt(35, 2, -1);
 
 // create a renderer
 const renderer = new THREE.WebGLRenderer( { alpha: true });
