fa78a83cd3b969b7d4fd7a0c63f4f57f4ca64197
510home <daniel@danielcardozo.com>
2026-04-28 22:41:45 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -137,7 +137,7 @@ time = currentTime
 //   wheelsFront.rotation.z += 0.01;
 //  };
 //if (wheelsFront) {wheelsFront.rotation.z = t * Math.PI * 2 * 9;
-wheelsBack.rotation.z += 0.02 * deltaTime;
+wheelsBack.rotation.z -= 0.02 * deltaTime;
 // controls.update();
 renderer.render(scene, camera);
  }
