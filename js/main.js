ca9017bfb40ba3e690ca6d376ffedafdedd54ada
510home <daniel@danielcardozo.com>
2026-04-29 00:33:00 -0700

    Adjust camera position and lookAt parameters

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ scene.fog = new THREE.Fog(0x000000, 2, 10);
 
 //create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(1.5, 0.5, 1.2);
-    camera.lookAt(2, 0, -1);
+    camera.position.set(1.5, 0.75, 1.2);
+    camera.lookAt(2, 0, -2);
 
 // create a renderer
 const renderer = new THREE.WebGLRenderer( { alpha: true });
