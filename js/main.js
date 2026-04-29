bb05f3875b811b50bd4b3f596e98f1e5fb961ab7
510home <daniel@danielcardozo.com>
2026-04-29 08:24:29 -0700

    Adjust camera position and lookAt target
    Adjusting camera to get the car framed in the center

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ scene.fog = new THREE.Fog(0x000000, 2, 10);
 
 //create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(1.3, 0.85, 1);
-    camera.lookAt(5, 1, -2);
+    camera.position.set(1, 0.85, 2);
+    camera.lookAt(8, 1, -2);
 
 // create a renderer
 const renderer = new THREE.WebGLRenderer( { alpha: true });
