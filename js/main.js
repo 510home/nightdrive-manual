bef905e7e968b5459620e678e4ceb3c4c34e4da1
510home <daniel@danielcardozo.com>
2026-04-29 10:32:55 -0700

    Update camera lookAt position in main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ scene.fog = new THREE.Fog(0x000000, 2, 10);
 //create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(1, 1, 2);
-    camera.lookAt(15, 12, 1);
+    camera.lookAt(25, 12, 1);
 
 // create a renderer
 const renderer = new THREE.WebGLRenderer( { alpha: true });
