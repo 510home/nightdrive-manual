c7da7861c0bf403b2c1476c8761d7b8b53e8947c
510home <daniel@danielcardozo.com>
2026-04-29 00:01:47 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ scene.fog = new THREE.Fog(0x000000, 2, 10);
 
 //create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(1.25, 1, 2);
+    camera.position.set(1.25, .25, 1);
     camera.lookAt(0, 0.5, 0);
 
 // create a renderer
