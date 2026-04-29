ae732da19c7f22819c5651a7e8b869f0b7f40115
510home <daniel@danielcardozo.com>
2026-04-29 12:22:01 -0700

    Modify circle geometry and moon position
    Updated circle geometry resolution and adjusted moon position.

--- js/main.js
+++ js/main.js
@@ -34,9 +34,10 @@ controls.maxDistance = 20;
 const meshLoader = new GLTFLoader();
 
 //Synthwave Moon ────────────────────── a circle for the camera to look at
-const circle = new THREE.CircleGeometry( 20, 10, 100 );
+const circle = new THREE.CircleGeometry( 20, 36);
 const moonmat = new THREE.MeshBasicMaterial( { color: 0xff66ff } );
-const moon = new THREE.Mesh( circle, moonmat );
+const moon = new THREE.Mesh( circle, moonmat);
+moon.position.set(12, 2, 20)
 scene.add( moon )
 
 
