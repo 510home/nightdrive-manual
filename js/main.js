dda415d12606461b645f34d5bbdce7136ac71a7b
510home <daniel@danielcardozo.com>
2026-05-05 17:09:55 -0700

    Adjust bloom pass strength in main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ const renderer = new THREE.WebGLRenderer( { alpha: true });
     composer.addPass(renderScene);
 // BLOOM PASS SETTINGS ------ Synthwave Glow
     const bloomPass = new UnrealBloomPass(
-      new THREE.Vector2(window.innerWidth, window.innerHeight), 2, 4, 0.75);
+      new THREE.Vector2(window.innerWidth, window.innerHeight), 1, 4, 0.75);
    //  bloomPass.threshold = 0;
    //  bloomPass.strength = 0.5;
    //  bloomPass.radius = 0;
