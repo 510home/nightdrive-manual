414c0ee7e0be3d10c221e8b7b24a4975302d750d
510home <daniel@danielcardozo.com>
2026-05-05 17:06:09 -0700

    Adjust bloom pass strength in main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ const renderer = new THREE.WebGLRenderer( { alpha: true });
     const bloomPass = new UnrealBloomPass(
       new THREE.Vector2(window.innerWidth, window.innerHeight), 5, 6, 0.75);
    //  bloomPass.threshold = 0;
-   //  bloomPass.strength = 1.5;
+   //  bloomPass.strength = 0.5;
    //  bloomPass.radius = 0;
     composer.addPass(bloomPass);
 
