200f0cbc4f306fe22afd420d4aad8f54a5477ad4
510home <daniel@danielcardozo.com>
2026-06-10 15:49:28 -0700

    Adjust polar angle limits for controls

--- js/main.js
+++ js/main.js
@@ -54,8 +54,8 @@ controls.maxDistance = 6;
 controls.target = new THREE.Vector3(0, .75, -1);
 controls.maxAzimuthAngle = THREE.MathUtils.degToRad(20);
 controls.minAzimuthAngle = THREE.MathUtils.degToRad(-20);
-controls.maxPolarAngle = THREE.MathUtils.degToRad(-120); 
-controls.minPolarAngle = THREE.MathUtils.degToRad(-90);
+controls.maxPolarAngle = THREE.MathUtils.degToRad(120); 
+controls.minPolarAngle = THREE.MathUtils.degToRad(150);
 
 
 // MOON ────────────────────── 
