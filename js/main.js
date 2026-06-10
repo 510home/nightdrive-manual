8cad5368e042819c0d70fb28afac150b02a1dda7
510home <daniel@danielcardozo.com>
2026-06-10 01:27:02 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ scene.background = new THREE.Color(0x000022);
 //scene.fog = new THREE.Fog(0x000000, 1, 12);
 
 //create a camera
-const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 50);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 50);
     camera.position.set(5, 0.5, 0);
     camera.lookAt(0, 0.5, -4);
 
