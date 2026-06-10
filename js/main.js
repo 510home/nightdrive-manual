054a63831aca2dee9eb8421843ed90ed875a202b
510home <daniel@danielcardozo.com>
2026-06-10 15:01:39 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -54,8 +54,8 @@ controls.maxDistance = 6;
 controls.target = new THREE.Vector3(0, .75, -1);
 controls.maxAzimuthAngle = THREE.MathUtils.degToRad(60);
 controls.minAzimuthAngle = THREE.MathUtils.degToRad(10);
-controls.maxPolarAngle = THREE.MathUtils.degToRad(110);
-controls.minPolarAngle = THREE.MathUtils.degToRad(80);
+controls.maxPolarAngle = THREE.MathUtils.degToRad(30);
+controls.minPolarAngle = THREE.MathUtils.degToRad(-20);
 
 
 // MOON ────────────────────── 
